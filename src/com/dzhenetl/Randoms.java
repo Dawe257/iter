b2adb96bae15e5b0eb3ba469973b853a06ac24f8
David Zhenetl <david.zhenetl@gmail.com>
2022-07-12 09:15:27 +0300

    убрал лист

--- src/com/dzhenetl/Randoms.java
+++ src/com/dzhenetl/Randoms.java
@@ -1,43 +1,32 @@
 package com.dzhenetl;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
 
     private final Random random;
-    private final List<Integer> numbers;
     private final int min;
     private final int max;
 
     public Randoms(int min, int max) {
         random = new Random();
-        numbers = new ArrayList<>();
         this.min = min;
         this.max = max + 1;
-        numbers.add(random.nextInt(this.min, this.max));
-    }
-
-    private void addRandomNumber() {
-        numbers.add(random.nextInt(this.min, this.max));
     }
 
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<>() {
-            int index = 0;
 
             @Override
             public boolean hasNext() {
-                return index < numbers.size();
+                return true;
             }
 
             @Override
             public Integer next() {
-                addRandomNumber();
-                return numbers.get(index++);
+                return random.nextInt(min, max);
             }
         };
     }
